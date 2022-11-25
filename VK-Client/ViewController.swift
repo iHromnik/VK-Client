b80ef42c529e36daf09b16551b0c3d37acc81d92
iHromnik <hromnik@me.com>
2022-11-25 14:17:07 +0200

    Initial Commit

---
+++ VK-Client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK-Client
+//
+//  Created by HappyRoman on 25/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
