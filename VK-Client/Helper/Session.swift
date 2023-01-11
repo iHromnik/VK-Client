79534e3d751e15b3b3898ccca2ab2ff6eb30c523
iHromnik <hromnik@me.com>
2023-01-11 16:26:21 +0200

    add singleton Session

---
+++ VK-Client/Helper/Session.swift
@@ -0,0 +1,15 @@
+//
+//  Session.swift
+//  VK-Client
+//
+//  Created by HappyRoman on 11/01/2023.
+//
+
+import UIKit
+
+class Session {
+    static let instance = Session()
+    private init() {}
+    let token: String = ""
+    let userID: Int = 0
+}
